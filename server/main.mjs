afee38d53c95371172cd0a7b1dfc7cead1278e24
Tim Havlicek <tim.h4vlicek@gmail.com>
2021-06-05 06:19:58 +0200

    rooms

--- server/main.mjs
+++ server/main.mjs
@@ -11,17 +11,45 @@ async function main() {
     const server = http.Server(app);
     const io = new IoServer(server);
 
-    const senders = new Set();
+    const senders = new Map();
+
+    function getSenders(id) {
+        if(!senders.has(id)) {
+            senders.set(id, new Set());
+        }
+        return senders.get(id);
+    }
+
+    function boradcast(id, event, data) {
+        const senders = getSenders(id);
+        for(let sender of senders) {
+            sender.send(event, data);
+        }
+    }
+
+    function handleSender(socket) {
+        const senders = getSenders(socket.roomId);
+        senders.add(socket);
+    }
+
+    function handleDisconnect(socket) {
+        const senders = getSenders(socket.roomId);
+        if(senders.has(socket)) {
+            senders.delete(socket);
+            console.log('sneder diconnected');
+        }
+    }
 
     const requests = {};
 
     io.on("connection", (socket) => {
         socket.on('disconnect', e => {
-            if(senders.has(socket)) {
-                senders.delete(socket);
-                console.log('sneder diconnected');
-            }
+            handleDisconnect(socket);
         });
+
+        const originHref = new URL(socket.handshake.headers.referer);
+        const roomId = originHref.search.slice(1);
+        socket.roomId = roomId;
 
         socket.on("message", (type, data) => {
             switch(type) {
@@ -34,9 +62,7 @@ async function main() {
                             id: reqid
                         });
                     }
-                    for(let sender of senders) {
-                        sender.send('createOffer', { id: reqid });
-                    }
+                    boradcast(socket.roomId, 'createOffer', { id: reqid });
                     break;
                 case 'offer':
                     if(requests[data.id]) {
@@ -44,12 +70,10 @@ async function main() {
                     }
                     break;
                 case 'answer':
-                    for(let sender of senders) {
-                        sender.send('answer', data);
-                    }
+                    boradcast(socket.roomId, 'answer', data);
                     break;
                 case 'boradcast':
-                    senders.add(socket);
+                    handleSender(socket);
                     console.log("sneder connected");
                     break;
             }
